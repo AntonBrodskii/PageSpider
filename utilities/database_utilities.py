6ff19c13ecf829d748ea4aa0460cbc229cd3c5a2
trump68 <trump68@list.ru>
2020-12-08 22:15:30 +0300

    Initial commit

---
+++ utilities/database_utilities.py
@@ -0,0 +1,7 @@
+def create_database(databasepath: str):
+    #TODO: add code
+    pass
+
+def save_words_to_database(database_path:str, words_list: list):
+    #TODO: add code
+    pass
---
+++ utilities/url_utilities.py
@@ -0,0 +1,11 @@
+def load_urls_from_file(file_path: str):
+    #TODO: add the code needed to read text file with the urls in it
+    pass
+
+def load_page(url:str):
+    #TODO: add code here
+    pass
+
+def scrape_page(page_contents: str):
+    #TODO: add code here
+    pass
