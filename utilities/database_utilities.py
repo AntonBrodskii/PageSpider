c1c08933ea8e8f2e9faeaf0b23c79580e0d77aee
trump68 <trump68@list.ru>
2020-12-15 14:09:55 +0300

    added code to page spider to call url_utilities

--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -1,8 +1,8 @@
-def create_database(databasepath: str):
-    # TODO: add code
+def create_database(database_path: str):
+    # TODO: generate the database
     pass
 
 
 def save_words_to_database(database_path: str, words_list: list):
-    # TODO: add code
+    # TODO: save the words to the database
     pass
